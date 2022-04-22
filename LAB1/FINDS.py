0abb132fb1802e1731c74f93c8ba07ac57b41be7
sakshi1bm19cs140 <71483959+sakshi1bm19cs140@users.noreply.github.com>
2022-04-22 12:15:01 +0530

    Create FINDS.py

---
+++ LAB1/FINDS.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+ 
+data = pd.read_csv("/content/drive/MyDrive/Machinelearning/weather.csv")
+print(data,"\n")
+ 
+#array of all the attributes
+d = np.array(data)[:,:-1]
+print("\n The attributes are: ",d)
+ 
+target = np.array(data)[:,-1]
+print("\n The target is: ",target)
+
+global specific_hypothesis 
+def findS(c,t):
+    for i, val in enumerate(t):
+        if val == "Yes":
+            specific_hypothesis = c[i].copy()
+            break
+             
+    for i, val in enumerate(c):
+        if t[i] == "Yes":
+            for x in range(len(specific_hypothesis)):
+                if val[x] != specific_hypothesis[x]:
+                    specific_hypothesis[x] = '?'
+                else:
+                    pass
+                 
+    return specific_hypothesis
+ 
+print("\n The final hypothesis is:",findS(d,target))
